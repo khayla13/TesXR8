469ae19d980e35cc7d45db05d403399f71ffe61f
khayla13 <qaisarah_fikri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 17:56:00 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,40 @@
+public class BukuTester {
+    public static void main(String[] args) {
+        Buku CaraAgarLucu = new Buku(1, "CaraAgarLucu", "Keylaa", 2021);
+    Buku CaraAgarCantik = new Buku(2, "CaraAgarCantik", "Qaisarah", 1999);
+    Buku CaraAgarManis = new Buku (3, "CaraAgarManis", "kela", 1890);
+    Buku CaraAgarImut = new Buku(4, "CaraAgarImut", "Khayla", 1988);
+    Buku CaraAgarTinggi = new Buku(5, "CaraAgarTinggi", "POPO", 2024);
+
+  //CaraAgarLucu
+    System.out.println(CaraAgarLucu.id);
+    System.out.println(CaraAgarLucu.judul);
+    System.out.println(CaraAgarLucu.pengarang);
+    System.out.println(CaraAgarLucu.Tahunterbit);
+
+  //CaraAgarCantik
+     System.out.println(CaraAgarCantik.id);  
+     System.out.println(CaraAgarCantik.judul);  
+     System.out.println(CaraAgarCantik.pengarang);  
+     System.out.println(CaraAgarCantik.Tahunterbit);  
+
+  //CaraAgarManis
+    System.out.println(CaraAgarManis.id);
+    System.out.println(CaraAgarManis.judul);
+    System.out.println(CaraAgarManis.pengarang);
+    System.out.println(CaraAgarManis.Tahunterbit);
+
+  //CaraAgarTinggi
+    System.out.println(CaraAgarTinggi.id);
+    System.out.println(CaraAgarTinggi.judul);
+    System.out.println(CaraAgarTinggi.pengarang);
+    System.out.println(CaraAgarTinggi.Tahunterbit); {
+
+ }
+
+
+
+ 
+    }
+    
+}
